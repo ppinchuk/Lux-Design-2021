317b3c1efbdd4959f66e381068555a760fd6d27d
paul <ppinchuk@physics.ucla.edu>
2021-09-22 11:43:43 -0600

    Updated submission output directories

--- kits/python/simple/compile_submission.py
+++ kits/python/simple/compile_submission.py
@@ -3,10 +3,18 @@ import time
 import sys
 
 if __name__ == '__main__':
+    timestr = time.strftime("%m_%d_%Y-%H-%M-%S")
 
     try:
         subprocess.run(
-            ['tar', '-czvf', 'submission.tar.gz',
+            ['tar', '-czvf', 'C:\\Users\\Paul\\OneDrive\\LuxAIReplays\\submission.tar.gz',
+             '__init__.py', 'agent.py', 'main.py', 'lux'],
+            cwd='C:\\Users\\Paul\\PycharmProjects\\Lux-Design-2021\\kits\\python\\simple',
+            shell=True
+        )
+
+        subprocess.run(
+            ['tar', '-czvf', f'C:\\Users\\Paul\\OneDrive\\LuxAIReplays\\past_submissions\\submission_{timestr}.tar.gz',
              '__init__.py', 'agent.py', 'main.py', 'lux'],
             cwd='C:\\Users\\Paul\\PycharmProjects\\Lux-Design-2021\\kits\\python\\simple',
             shell=True
